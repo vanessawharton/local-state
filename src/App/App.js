a6491d51a1751cc731a24d58bbab9b6ae5a79c75
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-11-21 09:23:28 -0600

    Added App folder

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App/App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      Header();
+      <Header />
     </div>
   );
 }
